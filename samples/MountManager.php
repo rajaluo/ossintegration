ced49f421430d2bbcd8c2e2c45320df5a9ccdbfc
luolei <luolei@lemediaweb.com>
2017-07-03 11:14:47 +0800

    save

--- samples/MountManager.php
+++ samples/MountManager.php
@@ -1,10 +1,14 @@
 <?php
+namespace ossintegration;
 
-use ossintegration;
-$local = new ossintegration\Adapter\Local();
-$ossAliyun = new ossintegration\Adapter\Oss('qukan-test', 'SBns8nHSboF7cItp', 'sWuCgC58sEfbugEQknj46kaxTqn4r2', 'http://oss-cn-beijing.aliyuncs.com');
+//use ossintegration;
+use ossintegration\Adapter\Local;
+use ossintegration\Adapter\Oss;
+
+$local = new Local();
+$ossAliyun = new Oss('qukan-test', 'SBns8nHSboF7cItp', 'sWuCgC58sEfbugEQknj46kaxTqn4r2', 'http://oss-cn-beijing.aliyuncs.com');
 $fileSys = ['local' => $local, 'oss' => $ossAliyun];
-$manager = new ossintegration\MountManager($fileSys);
+$manager = new MountManager($fileSys);
 $mount = $manager->mountFilesystem('local');
 
 $filename = '/data/oss/localdata';
